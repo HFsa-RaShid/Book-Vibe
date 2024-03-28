97e4fa28500fe8c6f4608cf5c3f75a18d4484ca9
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-03-28 22:32:16 +0600

    fiction route added

--- src/components/Fiction/FictionBooks.jsx
+++ src/components/Fiction/FictionBooks.jsx
@@ -1,38 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import FictionReadBookCard from "../FictionReadBookCard/FictionReadBookCard";
+
 
 const FictionBooks = () => {
 
     const [bookCard, setBookCard] = useState([]);
-    const [readBookCard, setReadBooksCard] = useState([]);
-    const [wishListCard, setWishlistCard] = useState([]);
-  
-    const [tabIndex, setTabIndex] = useState(0);
-    useEffect(() => {
-      const storedJsonReadBookInfo = localStorage.getItem('book-read');
-      const storedReadBooks = JSON.parse(storedJsonReadBookInfo) || [];
-  
-  
-      const storedJsonWishlistBookInfo = localStorage.getItem('book-wishlist');
-      const storedWishlistBooks = JSON.parse(storedJsonWishlistBookInfo) || []; 
-     
-      fetch('../books.json')
-        .then(res => res.json())
-        .then(data => setBookCard(data));
-  
-      const staticReadBookCard = [];
-      for (const book of bookCard) {
-        for (const storedReadBook of storedReadBooks) {
-          if (book.bookId === parseInt(storedReadBook)) {
-            staticReadBookCard.push(book);
-          }
+  const [readBookCard, setReadBooksCard] = useState([]);
+
+  const [tabIndex, setTabIndex] = useState(0);
+  useEffect(() => {
+    const storedJsonReadBookInfo = localStorage.getItem('book-read');
+    const storedReadBooks = JSON.parse(storedJsonReadBookInfo) || [];
+
+
+    fetch('../books.json')
+      .then(res => res.json())
+      .then(data => setBookCard(data));
+
+    const staticReadBookCard = [];
+    for (const book of bookCard) {
+      for (const storedReadBook of storedReadBooks) {
+        if (book.bookId === parseInt(storedReadBook)) {
+          staticReadBookCard.push(book);
         }
       }
-  
-      setReadBooksCard(staticReadBookCard);
-      
+    }
+
+    setReadBooksCard(staticReadBookCard);
+
+  }, [bookCard]);
+
+
     return (
-        <div>
-            
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 container mx-auto">
+            {
+                           
+                 readBookCard.map(readBook =>
+                    readBook.category === "Fiction" && 
+                 <FictionReadBookCard key={readBook.bookId} readBook={readBook}></FictionReadBookCard>)
+             }
 
             
         </div>
---
+++ src/components/FictionReadBookCard/FictionReadBookCard.jsx
@@ -0,0 +1,36 @@
+
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import { RiPagesLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+const FictionReadBookCard = ({readBook}) => {
+    const {bookId,image,bookName,author}=readBook;
+    return (
+        <div className="mb-10 mt-10 container mx-auto">
+        
+        <div className="lg:flex border rounded-xl lg:h-[275px] gap-6">
+            <div className="p-5 lg:w-[270px] ">
+                <img src={image} className="h-full w-full bg-[#1313130D] rounded-xl" />
+            </div>
+
+            <div className="p-14">
+                <h1 className="text-[24px] font-bold fonts text-center">{bookName}</h1>
+                <hr className="my-2"></hr>
+                <p className="text-[#13131399] font-semibold pb-2 pt-3 text-center">By: {author}</p>
+                <hr className="mt-2 mb-4"></hr>
+                <div className="flex justify-center">
+                <Link to={`/book/${bookId}`}>
+                    <button className=" py-2 px-4 bg-[#23BE0A] text-white rounded-2xl">View Details</button>
+                    </Link>
+
+                </div>
+                
+                
+            </div>
+            
+        </div>
+    </div>
+    );
+};
+
+export default FictionReadBookCard;
